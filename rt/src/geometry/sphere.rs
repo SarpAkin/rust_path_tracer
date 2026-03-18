0f98567c9de1651235c7c2f41ce1e7d291b22228
Sarp Akın <sarp4.akin@gmail.com>
2026-03-18 21:18:05 +0300

    fixed sphere ray intersection

--- rt/src/geometry/sphere.rs
+++ rt/src/geometry/sphere.rs
@@ -1,17 +1,20 @@
 use glam::Vec3;
 
-use crate::{HitMaterial, Ray, RayHit, geometry::{AlwaysHit, DynGeometry, Geometry, GeometryContainer}, util::solve_quadratic};
-
+use crate::{
+	HitMaterial, Ray, RayHit,
+	geometry::{AlwaysHit, DynGeometry, Geometry, GeometryContainer},
+	util::solve_quadratic,
+};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Sphere {
 	pub center: Vec3,
 	pub radius: f32,
-	pub color:Vec3,
+	pub color: Vec3,
 }
 
 impl Sphere {
-	pub fn new(c: Vec3, r: f32,color:Vec3) -> Sphere { Self { center: c, radius: r ,color} }
+	pub fn new(c: Vec3, r: f32, color: Vec3) -> Sphere { Self { center: c, radius: r, color } }
 
 	pub fn bundle_spheres(spheres: Vec<Sphere>) -> impl Geometry {
 		DynGeometry::new(GeometryContainer::new(AlwaysHit, spheres))
@@ -23,19 +26,26 @@ impl Geometry for Sphere {
 		let delta = ray.origin() - self.center;
 		let delta_cos_dir = delta.dot(ray.direction());
 
-		solve_quadratic(1.0, 2.0 * delta_cos_dir, delta.length_squared() - self.radius * self.radius).map(|roots| {
-			let t = roots[0];
-			let normal = (delta + t * ray.direction()).normalize();
+		let roots = solve_quadratic(
+			1.0,
+			2.0 * delta_cos_dir, //
+			delta.length_squared() - self.radius * self.radius,
+		)?;
+		let t = roots[0];
+		if t < 0.0 {
+			return None;
+		}
 
-			RayHit {
-				ray,
-				t,
-				material: HitMaterial {
-					albedo: self.color, //
-					normal,
-					roughness:0.3,
-				},
-			}
+		let normal = (delta + t * ray.direction()).normalize();
+
+		Some(RayHit {
+			ray,
+			t,
+			material: HitMaterial {
+				albedo: self.color, //
+				normal,
+				roughness: 0.3,
+			},
 		})
 	}
 }
